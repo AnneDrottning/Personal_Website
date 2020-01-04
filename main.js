daa859d91aaa88151a02a2f03ec4ce7c1935b4cc
Anne Drottning <AnneDrottning@Annes-MacBook-Pro-147.local>
2020-01-04 16:32:35 +0100

    Added configurations for three types of clouds

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 'use strict'; // cannot use undeclared variables, code executed in strict mode
 
+// Create a global variable to determine the current weather background:
+var currWeather = 'cloudy'; // Always start with the clouds
+
 // Create the class to create the front page
 class FrontPage extends React.Component {
     // The constructor
-    constructor(props) {
-        super(props);
-    }
+    //constructor(props) {
+    //    super(props);
+    //}
 
     // Function to actually render the page:
     render() {
-        <div className="name">
-            <p className="theName">ANNE DROTTNING</p>
-        </div>
+        return (
+            <div>
+                {/* Display my name */}
+                <div className="name">
+                    <p className="theName">ANNE DROTTNING</p>
+                </div>
+                {/* Show the clouds */}
+                <div>
+                    <Element type="cloud" />
+                </div>   
+            </div> 
+        );
     }
 }
-//ReactDOM.render(<FrontPage />, document.getElementById('portfolio'));
+
+// Create the class for the elements on the screen
+class Element extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        if (this.props.type == "cloud") {
+            return (
+                <p className="fas fa-cloud cloud1"></p>
+            );
+        }
+        
+    }
+}
+
+ReactDOM.render(<FrontPage />, document.getElementById('root'));
