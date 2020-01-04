1a63df01af46c20336ec020007dbec79aa66c749
Anne Drottning <AnneDrottning@Annes-MacBook-Pro-147.local>
2020-01-04 15:45:12 +0100

    Added my name to the front page

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 'use strict'; // cannot use undeclared variables, code executed in strict mode
 
+// Create the class to create the front page
+class FrontPage extends React.Component {
+    // The constructor
+    constructor(props) {
+        super(props);
+    }
+
+    // Function to actually render the page:
+    render() {
+        <div className="name">
+            <p className="theName">ANNE DROTTNING</p>
+        </div>
+    }
+}
+//ReactDOM.render(<FrontPage />, document.getElementById('portfolio'));
